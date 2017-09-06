3027f25a5606ffb73097213c1809eda578772c89
vimalkrish <linuxvimal@gmail.com>
2017-09-06 15:13:10 +0530

    Update outputs.tf
    test

--- outputs.tf
+++ outputs.tf
@@ -7,25 +7,25 @@ output "prod-private-secondary-1b" {
 }
 
 output "prod-public-primary-1a" {
-  value = ["${aws_subnet.prod-private-secondary-1b.id}"]
+  value = ["${aws_subnet.prod-public-primary-1a.id}"]
 }
 
 output "prod-public-secondary-1b" {
-  value = ["${aws_subnet.prod-private-secondary-1b.id}"]
+  value = ["${aws_subnet.prod-public-secondary-1b.id}"]
 }
 
 output "vpc_id" {
   value = "${aws_vpc.mod.id}"
 }
 
-output "Hubzu-prod-public" {
-  value = "${aws_route_table.Hubzu-prod-public.id}"
+output "public" {
+  value = "${aws_route_table.public.id}"
 }
 
-output "Hubzu-prod-private-1a" {
-  value = "${aws_route_table.Hubzu-prod-private-1a.id}"
+output "private-1a" {
+  value = "${aws_route_table.private-1a.id}"
 }
 
-output "Hubzu-prod-private-1b" {
-  value = "${aws_route_table.Hubzu-prod-private-1b.id}"
+output "private-1b" {
+  value = "${aws_route_table.private-1b.id}"
 }
