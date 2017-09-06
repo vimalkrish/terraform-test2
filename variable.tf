5fb54e21300a58641ca5d3049b2827e18e51e67c
vimalkrish <linuxvimal@gmail.com>
2017-09-06 12:48:35 +0530

    Update variable.tf
    test

--- variable.tf
+++ variable.tf
@@ -10,27 +10,32 @@ variable "cidr" {
 
 variable "prod-private-primary-1a" {
   description = "prod-private-primary-1a"
-  default     = []
+  default     = ""
 }
 
 variable "prod-private-secondary-1b" {
   description = "prod-private-secondary-1b"
-  default     = []
+  default     = ""
 }
 
 variable "prod-public-primary-1a" {
   description = "prod-public-primary-1a"
-  default     = []
+  default     = ""
 }
 
 variable "prod-public-secondary-1b" {
   description = "prod-public-secondary-1b"
-  default     = []
+  default     = ""
 }
 
-variable "azs" {
-  description = "A list of Availability zones in the region"
-  default     = []
+variable "az-1a" {
+  description = "az-1a"
+  default     = "us-east-1a"
+}
+
+variable "az-1b" {
+  description = "az-1b"
+  default     = "us-east-1b"
 }
 
 variable "enable_dns_hostnames" {
